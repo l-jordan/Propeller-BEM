23cd606ac9dd78f97bc773770ca0c54c40e5883e
Lauren <127839534+l-jordan@users.noreply.github.com>
2024-08-16 11:38:35 +0100

    Update Propeller_BEM.py Input Class
    Correctly used the Input class to design propeller characteristics.

--- Propeller_BEM.py
+++ Propeller_BEM.py
@@ -7,21 +7,6 @@ import toml
 class Propeller_Thrust:
     def __init__(self, inp: Inputs):
         self.input = inp 
-        self.V = self.input.V #Forward velocity [m/s]
-        self.B = self.input.B #Number of blades
-        self.R = self.input.R #Radius of the rotor blade [m]
-        self.h = self.input.h #Altitude [m]
-        self.w = self.input.w #Rotational speed of the rotor [rad/s]
-        self.cl = self.input.cl #cl-alpha for alpha = 3deg
-        self.cd = self.input.cd #cl-cd for alpha = 3deg
-        self.c = self.input.c
-        self.a = self.input.a #Induction factor guess
-        self.ap = self.input.ap #Axial induction factor guess
-        self.solidity = self.input.solidity #Solidity of the blades
-        self.n = self.input.n #Number of propellers
-        self.graph = self.input.graph
-        self.Pa = self.input.Pa #Power available of one engine
-
         self.r = None #Radius of blade elements
         self.rho = None #Density at altitude
         self.vi = None #Induced velocity
@@ -38,7 +23,7 @@ class Propeller_Thrust:
         R = 287 #J/kg*K
         g = 9.80665 #m/s^2
 
-        T1 = T0 + a*self.h
+        T1 = T0 + a*self.input.h
         rho = rho0*(T1/T0)**(-(g/(a*R))-1)
         self.rho = rho
         return rho
@@ -49,19 +34,19 @@ class Propeller_Thrust:
 
         while diff_a > 0.05 and diff_ap > 0.05:
             #INFLOW ANGLE
-            inflow = np.arctan((self.V*(1+self.a))/(self.w*self.r*(1-self.ap)))
+            inflow = np.arctan((self.input.V*(1+self.input.a))/(self.input.w*self.r*(1-self.input.ap)))
 
 
-            ftip = (self.B/2)*(self.R - self.r)/(self.r*np.sin(inflow))
+            ftip = (self.input.B/2)*(self.input.R - self.r)/(self.r*np.sin(inflow))
             F = (2/np.pi)*np.arccos(np.exp(-ftip)) #tip correction
 
-            a_new = ((4*F*np.sin(inflow)**2)/(self.solidity*(self.cl*np.cos(inflow) - self.cd*np.sin(inflow))) -1)**(-1)
-            ap_new = ((4*F*np.sin(inflow)*np.cos(inflow))/(self.solidity*(self.cl*np.sin(inflow) - self.cd*np.cos(inflow)))+1)**(-1)
+            a_new = ((4*F*np.sin(inflow)**2)/(self.input.solidity*(self.input.cl*np.cos(inflow) - self.input.cd*np.sin(inflow))) -1)**(-1)
+            ap_new = ((4*F*np.sin(inflow)*np.cos(inflow))/(self.input.solidity*(self.input.cl*np.sin(inflow) - self.input.cd*np.cos(inflow)))+1)**(-1)
 
-            diff_a = (a_new - self.a)/self.a
-            diff_ap = (ap_new - self.ap)/self.ap
-            self.a = a_new
-            self.ap = ap_new
+            diff_a = (a_new - self.input.a)/self.input.a
+            diff_ap = (ap_new - self.input.ap)/self.input.ap
+            self.input.a = a_new
+            self.input.ap = ap_new
         return
 
     ############################    INDUCTION FACTORS ITERATION   ############################
@@ -74,10 +59,10 @@ class Propeller_Thrust:
             self.r = r 
             self.iteration()
             radius.append(r)
-            a.append(self.a)
-            ap.append(self.ap)
+            a.append(self.input.a)
+            ap.append(self.input.ap)
 
-        if self.graph == True:
+        if self.input.graph == True:
             fig, ax = plt.subplots(nrows=1, ncols=2)
             ax[0].plot(radius, a, label = 'Axial induction factor')
             ax[0].grid(True)
@@ -93,12 +78,12 @@ class Propeller_Thrust:
             ax[0].set_title('a vs r')
             ax[1].set_title(r'a prime vs r')
             
-            fig.suptitle(f'Axial & Tangential Induction factors for a Speed of {np.round(self.V*3.6)} km/hr')
+            fig.suptitle(f'Axial & Tangential Induction factors for a Speed of {np.round(self.input.V*3.6)} km/hr')
             plt.show()
 
         self.r = radius
-        self.a = a
-        self.ap = ap
+        self.input.a = a
+        self.input.ap = ap
         return
 
     ############################       THRUST OF THE ROTOR       #############################
@@ -109,24 +94,24 @@ class Propeller_Thrust:
         V_ind = []
         F_cor = []
         twist_opt = []
-        for i in np.arange(0, len(self.a)):
+        for i in np.arange(0, len(self.input.a)):
             dr = 0.05
             #INFLOW ANGLE
-            inflow = np.arctan((self.V*(1+self.a[i]))/(self.w*self.r[i]*(1-self.ap[i])))
+            inflow = np.arctan((self.input.V*(1+self.input.a[i]))/(self.input.w*self.r[i]*(1-self.input.ap[i])))
 
-            ftip = (self.B/2)*(self.R - self.r[i])/(self.r[i]*np.sin(inflow))
+            ftip = (self.input.B/2)*(self.input.R - self.r[i])/(self.r[i]*np.sin(inflow))
             F = (2/np.pi)*np.arccos(np.exp(-ftip)) #tip correction
 
-            dT = 4*np.pi*(self.r[i])*self.rho*((self.V)**2)*(1+self.a[i])*self.a[i]*F*dr
+            dT = 4*np.pi*(self.r[i])*self.rho*((self.input.V)**2)*(1+self.input.a[i])*self.input.a[i]*F*dr
 
-            dQ = self.rho*4*np.pi*((self.r[i])**3)*(self.V)*(1+self.a[i])*self.ap[i]*self.w*F*dr
+            dQ = self.rho*4*np.pi*((self.r[i])**3)*(self.input.V)*(1+self.input.a[i])*self.input.ap[i]*self.input.w*F*dr
 
-            V0 = self.V*(1+self.a[i])
-            V2 = self.w*self.r[i]*(1-self.ap[i])
+            V0 = self.input.V*(1+self.input.a[i])
+            V2 = self.input.w*self.r[i]*(1-self.input.ap[i])
 
             V1 = np.sqrt(V0**2 + V2**2)
-            dD = self.B*self.rho*0.5*(V1**2)*self.cd*0.5
-            Vind = self.V*(1+self.a[i]*F)
+            dD = self.input.B*self.rho*0.5*(V1**2)*self.input.cd*self.input.c
+            Vind = self.input.V*(1+self.input.a[i]*F)
             
             alpha = 3 #degrees
             inflow = inflow*(180/np.pi)
@@ -136,11 +121,11 @@ class Propeller_Thrust:
             Q.append(dQ)
             D.append(dD)
 
-        Pr = (self.n*sum(Q)*self.w + self.n*sum(D))/1000 
-        Q = np.round(sum(Q)*self.n/1000, 2)
-        T = np.round(sum(T)*self.n/1000, 2)
+        Pr = (self.input.n*sum(Q)*self.input.w + self.input.n*sum(D))/1000 
+        Q = np.round(sum(Q)*self.input.n/1000, 2)
+        T = np.round(sum(T)*self.input.n/1000, 2)
 
-        if self.graph == True: 
+        if self.input.graph == True: 
             fig, (ax1, ax2) = plt.subplots(1, 2)
             ax1.plot(self.r, F_cor)
             ax1.set_xlabel('Radius [m]')
@@ -156,16 +141,16 @@ class Propeller_Thrust:
             plt.show()
 
         #Excess power remaining to overcome drag and torque needed
-        excess = self.Pa*self.n - Pr
+        excess = self.input.Pa*self.input.n - Pr
 
-        return Pr, T, Q, twist_opt, excess, self.V
+        return Pr, T, Q, twist_opt, excess, self.input.V
     
     def callallnames(self):
         self.altitude()
         self.induction_factors()
-        Pr, T, Q, twist_opt, excess, self.V = self.thrust()
+        Pr, T, Q, twist_opt, excess, self.input.V = self.thrust()
 
-        return Pr, T, Q, twist_opt, excess, self.V
+        return Pr, T, Q, twist_opt, excess, self.input.V
     
 if __name__ == '__main__':
    ############################       REPLACE INPUTS HERE       ############################# 
